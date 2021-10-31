63a236d42d56c6fcc8fc06342625deb95475e82d
Dark-knight-dust <73741638+Dark-knight-dust@users.noreply.github.com>
2021-10-31 19:53:09 +0530

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+#define fast ios::sync_with_stdio(false), cin.tie(0);
+#define ll long long
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define endl '\n'
+
+const int N = 1e5+10;
+
+using namespace std;
+
+void solve()
+{
+  cout<<"Test"<<endl;
+}
+
+int main()
+{
+  int t;
+  cin>>t;
+  while (t--)
+  {
+    solve();
+  }
+  return 0;
+}
